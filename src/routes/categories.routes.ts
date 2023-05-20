1e62106cebd499ac6b93b0afbacb7eef7dac7260
vsantos1711 <vsantos067100@gmail.com>
2023-05-20 15:43:41 -0300

    ImportCategoryController

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router, response } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRouter = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRouter.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -10,6 +15,10 @@ categoriesRouter.post("/", (request, response) => {
 
 categoriesRouter.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRouter.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRouter };
