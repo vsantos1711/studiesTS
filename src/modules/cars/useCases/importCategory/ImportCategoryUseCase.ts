9740e25d9ffa02708a9a00f5f5956c3b1b8370a9
vsantos1711 <vsantos067100@gmail.com>
2023-05-21 14:52:40 -0300

    ImportFiles

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,44 @@
+import fs from "fs";
+import csvParse from "csv-parser";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  constructor(private categoriesRespository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const { TYPE, DESCRIPTION } = line;
+
+          categories.push({
+            name: TYPE,
+            description: DESCRIPTION,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("erro", (err) => {
+          reject(err);
+        });
+    });
+  }
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
   }
 }
 
