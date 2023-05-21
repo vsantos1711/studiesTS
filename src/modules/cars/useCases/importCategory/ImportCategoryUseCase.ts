78a05bacd9d6696c79727e17c6e46e09321912b3
vsantos1711 <vsantos067100@gmail.com>
2023-05-21 18:29:27 -0300

    Remove File imported

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -21,14 +21,15 @@ class ImportCategoryUseCase {
 
       parseFile
         .on("data", async (line) => {
-          const { TYPE, DESCRIPTION } = line;
+          const { name, description } = line;
 
           categories.push({
-            name: TYPE,
-            description: DESCRIPTION,
+            name,
+            description,
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("erro", (err) => {
