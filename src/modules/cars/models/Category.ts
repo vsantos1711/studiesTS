974047bce333d32790e980f8699cb758a3dd90d4
vsantos1711 <vsantos067100@gmail.com>
2023-05-27 18:26:44 -0300

    update Models

--- src/modules/cars/models/Category.ts
+++ src/modules/cars/models/Category.ts
@@ -1,14 +1,6 @@
-import { v4 as uuidv4 } from "uuid";
-
 export class Category {
   id?: string;
   name: string;
   description: string;
-  created_at: Date;
-
-  constructor() {
-    if (!this.id) {
-      this.id = uuidv4();
-    }
-  }
+  created_at?: Date;
 }
